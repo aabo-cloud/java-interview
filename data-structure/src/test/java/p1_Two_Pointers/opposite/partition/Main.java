00b209cfe103bda98639c26e55c99a21e41c8228
soull1998 <soull1998@qq.com>
2022-06-14 22:52:46 +0800

    add operating system

---
+++ data-structure/src/test/java/p1_Two_Pointers/opposite/partition/Main.java
@@ -0,0 +1,51 @@
+package p1_Two_Pointers.opposite.partition;
+
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[] nums = {9, 7, 5, 3, 1, 1, 3, 5, 7, 9};
+        new Main().sortIntegers2(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    public void sortIntegers2(int[] A) {
+        quickSort(A, 0, A.length - 1);
+    }
+
+    private void quickSort(int[] A, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int left = start, right = end;
+        // key point 1: pivot is the value, not the index
+        int pivot = A[(start + end) / 2];
+        // key point 2: every time you compare left & right, it should be
+        // left <= right not left < right
+        while (left <= right) {
+            // 等于pivot的往右边运
+            while (left <= right && A[left] < pivot) {
+                left++;
+            }
+            // 等于pivot的往左边运
+            while (left <= right && A[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        quickSort(A, start, right);
+        quickSort(A, left, end);
+    }
+}
---
+++ data-structure/src/test/java/p1_Two_Pointers/opposite/partition/t5第k大元素/Solution.java
@@ -0,0 +1,50 @@
+package p1_Two_Pointers.opposite.partition.t5第k大元素;
+
+// O(n)
+public class Solution {
+    /**
+     * @param k: An integer
+     * @param nums: An array
+     * @return: the Kth largest element
+     */
+    public static int kthLargestElement(int k, int[] nums) {
+        // write your code here
+        int n = nums.length;
+        k = n - k;
+        return partition(k, nums, 0, n - 1);
+    }
+
+    public static int partition(int k, int[] nums, int start, int end) {
+
+        int left = start, right = end;
+        int pivot = nums[left + (right - left) / 2];
+        while (left <= right) {
+            while (left <= right && nums[left] < pivot) {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        if (k <= right) {
+            return partition(k, nums, start, right);
+        }
+        if (k >= left) {
+            return partition(k, nums, left, end);
+        }
+        return nums[k];
+    }
+
+    public static void main(String[] args) {
+        int[] test = {0, 1, 2, 3, 4, 5, 6};
+        System.out.println(kthLargestElement(3, test));
+    }
+
+}
